91f69c8fedb22a9de1db70e4da92ed8387008b21
AM067563 <AM067563@W1851667.northamerica.cerner.net>
2019-04-08 12:33:21 +0530

    Commit

---
+++ SpringBootLiquibaseIntegration/src/main/java/com/springbootliquibase/app/SpringBootLiquibaseIntegration.java
@@ -0,0 +1,20 @@
+package com.springbootliquibase.app;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//import com.springbootliquibase.app.entities.Employee;
+//import com.springbootliquibase.app.repository.EmployeeRepository;
+
+@SpringBootApplication
+public class SpringBootLiquibaseIntegration {
+//
+//	@Autowired
+//	private EmployeeRepository employeeRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLiquibaseIntegration.class, args);
+	}
+}
